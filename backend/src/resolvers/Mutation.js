67e5b7125464680f098b6d549a53e2a1703e6508
Sandra <alecsandra.serediuc@gmail.com>
2020-06-10 20:34:00 +0200

    Adding the resolvers for creating and queryng the data

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,4 +1,18 @@
 //when push and put data
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info){
+        //  TODO: verify if they are logged in
+
+        // get acces to the methods generated inside the prisma.graphql
+        const item = await ctx.db.mutation.createItem({
+            data:{
+                ...args
+            }
+        }, info);
+        console.log(item);
+        
+        return item
+    }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,13 @@
 //resolvers when you pull data
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db'),
+  // async items(parent, args, ctx, info) {
+  //   console.log('Getting Items!!');
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
+};
 
 module.exports = Query;
