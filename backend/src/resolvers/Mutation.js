1a5923b7c6d6341e5339920cb3c5c00fb7afda09
Sandra <alecsandra.serediuc@gmail.com>
2020-07-22 16:35:20 +0200

    uptdating the resolvers for updateitem

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,18 +1,37 @@
 //when push and put data
 const Mutations = {
-  async createItem(parent, args, ctx, info){
-        //  TODO: verify if they are logged in
-
-        // get acces to the methods generated inside the prisma.graphql
-        const item = await ctx.db.mutation.createItem({
-            data:{
-                ...args
-            }
-        }, info);
-        console.log(item);
-        
-        return item
-    }
-};
-
-module.exports = Mutations;
+    async createItem(parent, args, ctx, info) {
+      // TODO: Check if they are logged in
+  // get acces to the methods generated inside the prisma.graphql
+      const item = await ctx.db.mutation.createItem(
+        {
+          data: {
+            ...args,
+          },
+        },
+        info
+      );
+  
+      console.log(item);
+  
+      return item;
+    },
+    updateItem(parent, args, ctx, info) {
+      // first take a copy of the updates
+      const updates = { ...args };
+      // remove the ID from the updates
+      delete updates.id;
+      // run the update method
+      return ctx.db.mutation.updateItem(
+        {
+          data: updates,
+          where: {
+            id: args.id,
+          },
+        },
+        info
+      );
+    },
+  };
+  
+  module.exports = Mutations;
