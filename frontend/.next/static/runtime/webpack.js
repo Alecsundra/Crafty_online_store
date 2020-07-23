9957683a8a411fb610902dfd2878acb8d9ac0602
Sandra <alecsandra.serediuc@gmail.com>
2020-07-23 21:05:21 +0200

    Setting the pagination/items per page

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f2266904ee3bd05e35c8";
+/******/ 	var hotCurrentHash = "fd19c4fef91ffaabbc30";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
