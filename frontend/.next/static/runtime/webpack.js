88cec3cc4624e313c29ed09ca8d7c9ab8ade18b2
Sandra <alecsandra.serediuc@gmail.com>
2020-07-23 20:43:20 +0200

    Adding the items draft,preparing the pagination

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e7d56556d27ef254d0b0";
+/******/ 	var hotCurrentHash = "f2266904ee3bd05e35c8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
