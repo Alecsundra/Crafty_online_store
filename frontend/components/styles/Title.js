46186c6a2d4e4ecc79a2d15fe344bdd3992e0be7
Sandra <alecsandra.serediuc@gmail.com>
2020-07-24 18:03:52 +0200

    Edited in order to fit the main changes of styling

--- frontend/components/styles/Title.js
+++ frontend/components/styles/Title.js
@@ -3,15 +3,17 @@ import styled from 'styled-components';
 const Title = styled.h3`
   margin: 0 1rem;
   text-align: center;
-  transform: skew(-5deg) rotate(-1deg);
+  /* transform: skew(-5deg) rotate(-1deg); */
   margin-top: -3rem;
-  text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
+  text-shadow: 4px 4px 0 rgba(0, 0, 0, 0.2);
   a {
-    background: ${props => props.theme.red};
+    background: rgb(228,154,164);
+background: radial-gradient(circle, rgba(228,154,164,1) 0%, rgba(246,88,136,1) 53%, rgba(255,0,56,1) 100%);
     display: inline;
     line-height: 1.3;
     font-size: 4rem;
     text-align: center;
+    font-weight:100;
     color: white;
     padding: 0 1rem;
   }
