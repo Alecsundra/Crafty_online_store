391fcec8aa769514df8ba6e1366aa5304b40230a
Sandra <alecsandra.serediuc@gmail.com>
2020-07-24 18:02:44 +0200

    Edited the shape

--- frontend/components/styles/PriceTag.js
+++ frontend/components/styles/PriceTag.js
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
 
 const PriceTag = styled.span`
-  background: ${props => props.theme.red};
+  background: rgb(228,154,164);
+  background: radial-gradient(circle, rgba(228,154,164,1) 0%, rgba(246,88,136,1) 53%, rgba(255,0,56,1) 100%);
   transform: rotate(3deg);
   color: white;
   font-weight: 600;
-  padding: 5px;
+  padding: 10px;
   line-height: 1;
   font-size: 3rem;
   display: inline-block;
   position: absolute;
   top: -3px;
   right: -3px;
+  border-radius:50%;
+  text-shadow: 4px 4px 0 rgba(0, 0, 0, 0.3);
+
+  /* border: 1px solid ${props => props.theme.black}; */
 `;
 
 export default PriceTag;
